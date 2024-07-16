ef10c1038fde9b81851116cea420010d6f0cb0ca
matheus pinto <mat.vsp@gmail.com>
2024-07-16 15:00:24 -0300

    cleaned my code

--- coding/hidden_APIs.py
+++ coding/hidden_APIs.py
@@ -1,19 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-'''
-page = requests.get(url)
-
-#print(page.status_code)
-#print(page.headers)
-
-soup = BeautifulSoup(page.text, "lxml")
-commentaries = soup.find_all('div', class_="testimonial")
-for comment in commentaries:
-    print(comment.text.strip())
-    count += 1
-'''
 count = 0
 
 
